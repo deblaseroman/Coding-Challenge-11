045b85b2b460b889b584b890d917c2fbe3867d3e
roman deblase <rdeblase2000@gmail.com>
2024-10-27 21:02:30 -0400

    add event listener

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
+
+//2. add event listener 
+
 const productSelector = document.getElementById('product-selector');
 const quantityInput = document.getElementById('quantity');
 const totalPrice = document.getElementById('total-price');
-const productSelector = document.getElementById('product-selector');
+const placeOrder = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+
+function updateTotalPrice() {
+    let totalPrice = productPrice * quantity;
+}
+
+//3. calculate total price dynamically 
